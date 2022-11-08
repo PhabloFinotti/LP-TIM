35ee378477123b738c190ab627bace848239cb9b
Phablo Finotti <phablofinotti@gmail.com>
2022-11-07 23:47:57 -0300

    chore: Alterando parametros de build vite

---
+++ vite.config.cjs
@@ -0,0 +1,17 @@
+const path = require('path');
+const { defineConfig } = require('vite');
+
+module.exports = defineConfig({
+  root: path.resolve(__dirname, 'src'),
+  base: '',
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'src/index.html'),
+      },
+      output: {
+        dir: path.resolve(__dirname, 'dist'),
+      },
+    },
+  },
+});
